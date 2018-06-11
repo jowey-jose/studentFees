dcf3c5377788bc7e989e267c5d21665824d14bce
Unknown <josephmbuku17@gmail.com>
2018-06-11 17:44:16 +0300

    staged changes.

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/94384/student', function () {
+    return view('student');
+});
+Route::get('/94384/fees', function () {
+    return view('fees');
+});
